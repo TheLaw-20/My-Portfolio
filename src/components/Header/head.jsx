69f520816002e5fd7ee0aa97cd57705d5cbe66b1
TheLaw-20 <lawrence21maurice@gmail.com>
2026-01-12 05:36:14 +0200

    Add Google site verification meta tag to Head component

--- src/components/Header/head.jsx
+++ src/components/Header/head.jsx
@@ -13,6 +13,7 @@ export default function Head() {
 
   return (
     <>
+    <head><meta name="google-site-verification" content="0OldCycPwFglpu_B4vOJfoHsCF8hc2kLp80GZCTSuuU" /></head>
       {/* NAVBAR */}
       <nav className="navbar">
         <div className="nav-logo">
